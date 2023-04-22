e9f2acc496d96b150ad12f1131d80f6baf56026a
Vinícius <viniciusgolfera@gmail.com>
2023-04-22 12:31:07 -0300

    Função para montar o menu atraves de um arquivo csv

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,23 @@
+import csv
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        with open(source_path, "r") as source:
+            self.__data__ = list(csv.DictReader(source))
+
+        dishes = {}
+
+        for row in self.__data__:
+            dish = Dish(row['dish'], float(row['price']))
+            if row["dish"] not in dishes:
+                dishes[row['dish']] = dish
+
+            dishes[row['dish']].add_ingredient_dependency(
+                Ingredient(row['ingredient']), int(row['recipe_amount'])
+            )
+
+        self.dishes = set(dishes.values())
