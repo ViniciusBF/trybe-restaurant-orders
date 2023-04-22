ca9b4f3e9b86da180a20e787b039c29e88cd7318
Vinícius <viniciusgolfera@gmail.com>
2023-04-22 10:25:35 -0300

    Testes para funções da class Ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,4 +3,17 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    pass
+    teste1 = Ingredient("bacon")
+    teste2 = Ingredient("farinha")
+    teste3 = Ingredient("bacon")
+
+    assert repr(teste1) == "Ingredient('bacon')"
+    assert teste1.name == "bacon"
+    assert repr(teste2) == "Ingredient('farinha')"
+    assert teste2.name == "farinha"
+
+    assert teste1 == teste3
+    assert len(teste1.restrictions) == 2
+
+    assert hash(teste1) == hash(teste3)
+    assert hash(teste1) != hash(teste2)
